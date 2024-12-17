0ee521f22e2d476b2778ea0a29ec6dbfca7600eb
Ryan Chua <ryan.chua.2024@computing.smu.edu.sg>
2024-12-17 22:49:36 +0800

    First commit

---
+++ postgresql.js
@@ -0,0 +1,34 @@
+import postgresql from 'pg';
+
+const { Pool } = postgresql;
+
+export default (callback = null) => {
+    // NOTE: PostgreSQL creates a superuser by default on localhost using the OS username.
+    const pool = new Pool({
+      user: process.env.NODE_ENV === 'development' && (os.userInfo() || {}).username || '',
+      database: 'app',
+      password: '',
+      host: '127.0.0.1',
+      port: 5432,
+    });
+
+    const connection = {
+        pool,
+        query: (...args) => {
+            return pool.connect().then((client) => {
+            return client.query(...args).then((res) => {
+                client.release();
+                return res.rows;
+            });
+            });
+        },
+    };
+    
+    process.postgresql = connection;
+
+    if (callback) {
+        callback(connection);
+    }
+
+    return connection;
+  };
